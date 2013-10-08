c0b6f91808270d136645bd4cdc04aa8c363a2298
Jonathan Johnson <jon@crowdfavorite.com>
2013-10-08 10:03:11 -0600

    revamp example.rb

--- example.rb
+++ example.rb
@@ -1,57 +1,65 @@
 $:.push File.expand_path("../lib", __FILE__)
 require 'steps'
 
-confirm "This is not a vital question, right?"
+report "This is an example of 'steps' output"
 
-step "Update Something" do
+step "Single Level Task" do
   sleep 1
 end
 
-confirm "Is this a vital question?", :vital => true
-
-step "Do Something else" do
-  confirm "Is this a vital question?", :vital => true
+step "Custom Success Message" do
   sleep 1
-  step "Nested Something" do
-    sleep 1
-    step "Double! Again", :debug => true do
-      report "Something you probably want to know"
-      raise "Problem"
-      sleep 1
-    end
-    step "Double! Again", :debug => true do
-      sleep 1
-      raise "Problem"
-      report "Something you probably want to know"
-    end
-    step "Double! Nested Something" do
-      step "Triple (and vital)! Nested Something", :vital => true do
-        answer = retrieve "What is your favorite color?"
-        report "this is something important
-Another Line
-A third line"
-        report "Something else important"
-        step "Quad! Nested Something" do
-          sleep 1
-          if confirm "Is this a vital question?", :vital => true
-            step "resolution" do
-              sleep 1
-            end
-          end
-        end
-        sleep 1
-        step "favorite color" do
-          sleep 1
-          answer
-        end
-      end
-      step "Another Triple" do
-        sleep 1
-      end
-      sleep 1
-    end
-    sleep 1
-  end
+  "Yay!"
+end
+
+step "Task with an error" do
   sleep 1
   raise
+end
+
+step "Custom Error Message" do
+  sleep 1
+  raise "There was a problem"
+end
+
+step "Debug Mode", :debug => true do
+  sleep 1
+  raise "There was a problem"
+end
+
+step "Progress Messages" do
+  sleep 0.5
+  report "Progress 1"
+  sleep 0.5
+  report "Progress 2"
+  true
+end
+
+step "User Input" do
+  sleep 0.5
+  color = retrieve "What is your favorite color?"
+  sleep 0.5
+  if confirm "Are you sure?"
+    report "Favorite color is #{color}"
+  else
+    report "Favorite color is not #{color}"
+  end
+  sleep 0.5
+end
+
+step "Vital Questions" do
+  sleep 0.5
+  confirm "This question is vital?", :vital => true
+  report "You answered yes."
+  sleep 0.5
+end
+
+step "Nested Steps" do
+  sleep 0.5
+  step "Nested Step Level 1" do
+    sleep 0.5
+    step "Nested Step Level 2" do
+      sleep 0.5
+    end
+  end
 end
